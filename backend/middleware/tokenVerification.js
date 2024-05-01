34b292bac23151d6c9525fada4daaa5dd564b1ab
mst019 <mohamedsalah.trabelsi@yahoo.com>
2024-05-01 19:12:17 +0100

    my 3rd commit

--- backend/middleware/tokenVerification.js
+++ backend/middleware/tokenVerification.js
@@ -1,19 +1,11 @@
-const admin = require("../config/firebase-config");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+
 const secret = "txrsplkg1234@QsdASXdfSRV094";
 
 const decodeToken = (req, res, next) => {
   console.log("start verifying now...");
-  //console.log(req.headers);
+
   const jwtToken = req.headers.authorization.split(" ")[1];
-
-  // const decodeValue = await admin.auth().verifyIdToken(token);
-  // if (decodeValue) {
-  //   console.log("token valid");
-  //   return next();
-
-  //console.log("token: " + jwtToken);
 
   jwt.verify(jwtToken, secret, (err, user) => {
     if (err) {
@@ -24,8 +16,6 @@ const decodeToken = (req, res, next) => {
     console.log("token valid");
     next();
   });
-
-  //return res.status(200).send("Invalid token");
 };
 
 module.exports = decodeToken;
